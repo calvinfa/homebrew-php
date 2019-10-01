cc909daf60e2e2c430a605be520f3058e50dc9a9
calvinfa <51708327+calvinfa@users.noreply.github.com>
2019-10-01 09:37:07 +0200

    Update valet-php@7.1.rb

--- Formula/valet-php@7.1.rb
+++ Formula/valet-php@7.1.rb
@@ -149,7 +149,7 @@ class ValetPhpAT71 < Formula
       --with-mysql-sock=/tmp/mysql.sock
       --with-mysqli=mysqlnd
       --with-ndbm#{headers_path}
-      --with-openssl=#{Formula["openssl"].opt_prefix}
+      --with-openssl=#{Formula["openssl@1.1"].opt_prefix}
       --with-pdo-dblib=#{Formula["freetds"].opt_prefix}
       --with-pdo-mysql=mysqlnd
       --with-pdo-odbc=unixODBC,#{Formula["unixodbc"].opt_prefix}
@@ -167,7 +167,7 @@ class ValetPhpAT71 < Formula
       --with-xsl#{headers_path}
       --with-zlib#{headers_path}
       --with-imap=#{Formula["imap-uw"].opt_prefix}
-      --with-imap-ssl=#{Formula["openssl"].opt_prefix}
+      --with-imap-ssl=#{Formula["openssl@1.1"].opt_prefix}
     ]
 
     system "./configure", *args
